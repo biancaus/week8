b6d740fe2cdb133ff75333c04e2f68df543a76d5
biancaus <15204877@massey.ac.nz>
2016-05-02 11:17:25 +1200

    java

--- collision_detection/script.js
+++ collision_detection/script.js
@@ -9,3 +9,40 @@ Next, add the following features:
 - create an item that makes the puck smaller
 
 */
+
+var puck_x = 0;
+var puck_y = 0;
+var speed = 10;
+
+    // move puck down event listener
+    if (puck_y == 290){
+        document.getElementById('down').addEventListener('click', function() {
+        //puck_y += 10;
+        document.getElementById('puck').style.top = puck_y + 'px';
+      }
+    );
+      
+    }
+    else {
+        document.getElementById('down').addEventListener('click', function() {
+        puck_y += 20;
+        document.getElementById('puck').style.top = puck_y + 'px';
+      }
+    );
+              
+    }
+    // move puck up event listener
+    if (puck_y == 0) {
+        document.getElementById('up').addEventListener('click', function() {
+        puck_y -= 10;
+        document.getElementById('puck').style.top = puck_y + 'px';
+      }
+    );
+    }
+
+    // move puck down event listener
+      document.getElementById('down').addEventListener('click', function() {
+        puck_y += 10;
+        document.getElementById('puck').style.top = puck_y + 'px';
+      }
+    );
