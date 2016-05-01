aa0ebcacae65ca3271d19b507ed92a7a96fd3036
biancaus <15204877@massey.ac.nz>
2016-05-02 10:50:00 +1200

    add'l resources

---
+++ collision_detection/script.js
@@ -0,0 +1,11 @@
+/*
+
+Begin by following the steps in the workshop notes.
+
+Next, add the following features:
+- the gap goes red when the puck is over it
+- the puck can't go beyond the arena borders
+- create an item that makes the puck bigger
+- create an item that makes the puck smaller
+
+*/
